0b58a9be917961574b08e52c20c03d844b683918
ysbot <i@ysicing.me>
2022-08-25 22:10:14 +0800

    add执行api

--- executions.go
+++ executions.go
@@ -23,6 +23,10 @@ import (
 	"github.com/imroc/req/v3"
 )
 
+type ExecutionsService struct {
+	client *Client
+}
+
 type ProjectExecutions struct {
 	Page       int         `json:"page"`
 	Total      int         `json:"total"`
@@ -45,8 +49,82 @@ type Execution struct {
 	Progress   int       `json:"progress"`
 }
 
-type ExecutionsService struct {
-	client *Client
+type ExecutionsCreateMeta struct {
+	Project int    `json:"project"`
+	Name    string `json:"name"`
+	Code    string `json:"code"`
+	Begin   string `json:"begin"`
+	End     string `json:"end"`
+	Days    int    `json:"days"`
+
+	Lifetime string `json:"lifetime,omitempty"`
+	P0       string `json:"PO,omitempty"`
+	PM       string `json:"PM,omitempty"`
+	QD       string `json:"QD,omitempty"`
+	RD       string `json:"RD,omitempty"`
+}
+
+type ExecutionsCreateMsg struct {
+	ID             int         `json:"id"`
+	Project        int         `json:"project"`
+	Model          string      `json:"model"`
+	Type           string      `json:"type"`
+	Lifetime       string      `json:"lifetime"`
+	Budget         string      `json:"budget"`
+	Budgetunit     string      `json:"budgetUnit"`
+	Attribute      string      `json:"attribute"`
+	Percent        int         `json:"percent"`
+	Milestone      string      `json:"milestone"`
+	Output         string      `json:"output"`
+	Auth           string      `json:"auth"`
+	Parent         int         `json:"parent"`
+	Path           string      `json:"path"`
+	Grade          int         `json:"grade"`
+	Name           string      `json:"name"`
+	Code           string      `json:"code"`
+	Begin          string      `json:"begin"`
+	End            string      `json:"end"`
+	Realbegan      string      `json:"realBegan"`
+	Realend        string      `json:"realEnd"`
+	Days           int         `json:"days"`
+	Status         string      `json:"status"`
+	Substatus      string      `json:"subStatus"`
+	Pri            string      `json:"pri"`
+	Desc           string      `json:"desc"`
+	Version        int         `json:"version"`
+	Parentversion  int         `json:"parentVersion"`
+	Planduration   int         `json:"planDuration"`
+	Realduration   int         `json:"realDuration"`
+	Openedby       string      `json:"openedBy"`
+	Openeddate     time.Time   `json:"openedDate"`
+	Openedversion  string      `json:"openedVersion"`
+	Lasteditedby   string      `json:"lastEditedBy"`
+	Lastediteddate time.Time   `json:"lastEditedDate"`
+	Closedby       string      `json:"closedBy"`
+	Closeddate     interface{} `json:"closedDate"`
+	Canceledby     string      `json:"canceledBy"`
+	Canceleddate   interface{} `json:"canceledDate"`
+	Po             string      `json:"PO"`
+	Pm             string      `json:"PM"`
+	Qd             string      `json:"QD"`
+	Rd             string      `json:"RD"`
+	Team           string      `json:"team"`
+	ACL            string      `json:"acl"`
+	Whitelist      string      `json:"whitelist"`
+	Order          int         `json:"order"`
+	Deleted        string      `json:"deleted"`
+	Totalhours     int         `json:"totalHours"`
+	Totalestimate  int         `json:"totalEstimate"`
+	Totalconsumed  int         `json:"totalConsumed"`
+	Totalleft      int         `json:"totalLeft"`
+}
+
+type ExecutionsUpdateMeta struct {
+	ExecutionsCreateMeta
+	TeamMembers []string `json:"teamMembers,omitempty"`
+	Desc        string   `json:"desc,omitempty"`
+	ACL         string   `json:"acl,omitempty"`
+	Whitelist   []string `json:"whitelist,omitempty"`
 }
 
 func (s *ExecutionsService) ListByProject(id int64) (*ProjectExecutions, *req.Response, error) {
@@ -56,4 +134,46 @@ func (s *ExecutionsService) ListByProject(id int64) (*ProjectExecutions, *req.Re
 		SetResult(&pe).
 		Get(s.client.RequestURL(fmt.Sprintf("/projects/%d/executions", id)))
 	return &pe, resp, err
+}
+
+// Create 创建执行
+func (s *ExecutionsService) Create(id int, build ExecutionsCreateMeta) (*ExecutionsCreateMsg, *req.Response, error) {
+	var u ExecutionsCreateMsg
+	resp, err := s.client.client.R().
+		SetHeader("Token", s.client.token).
+		SetBody(&build).
+		SetResult(&u).
+		Post(s.client.RequestURL(fmt.Sprintf("/projects/%d/executions", id)))
+	return &u, resp, err
+}
+
+// DeleteByID 删除执行
+func (s *ExecutionsService) DeleteByID(id int) (*CustomResp, *req.Response, error) {
+	var u CustomResp
+	resp, err := s.client.client.R().
+		SetHeader("Token", s.client.token).
+		SetResult(&u).
+		Delete(s.client.RequestURL(fmt.Sprintf("/executions/%d", id)))
+	return &u, resp, err
+}
+
+// UpdateByID 修改执行
+func (s *ExecutionsService) UpdateByID(id int, exec ExecutionsUpdateMeta) (*ExecutionsCreateMsg, *req.Response, error) {
+	var u ExecutionsCreateMsg
+	resp, err := s.client.client.R().
+		SetHeader("Token", s.client.token).
+		SetBody(&exec).
+		SetResult(&u).
+		Put(s.client.RequestURL(fmt.Sprintf("/executions/%d", id)))
+	return &u, resp, err
+}
+
+// GetByID 获取执行详情
+func (s *ExecutionsService) GetByID(id int) (*ExecutionsCreateMsg, *req.Response, error) {
+	var u ExecutionsCreateMsg
+	resp, err := s.client.client.R().
+		SetHeader("Token", s.client.token).
+		SetResult(&u).
+		Get(s.client.RequestURL(fmt.Sprintf("/executions/%d", id)))
+	return &u, resp, err
 }
